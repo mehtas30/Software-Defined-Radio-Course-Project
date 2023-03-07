b0b99c6fc4d0d9f225b66563b7d07a10c6386910
Isaac Shoong <shoongi@mcmaster.ca>
2023-03-07 17:09:55 -0500

    Trying FM block

--- src/filter.cpp
+++ src/filter.cpp
@@ -52,3 +52,25 @@ void convolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::
 		}
 	}
 }
+
+void demodFM(const std::vector<float> &i_ds, const std::vector<float> &q_ds, std::vector<float> &demod, float p_i=0, float p_q=0) {
+	for (int i=0; i < i_ds.size(); i++) {
+		float currI = i_ds[i];
+		float currQ = q_ds[i];
+
+		float derivI = currI - p_i;
+		float derivQ = currQ - p_q;
+
+		if ((pow(currI,2) + pow(currQ,2)) == 0) {
+			demod.push_back(0);
+		}
+		else {
+			float numerator = currI*derivI - currQ*derivQ;
+			float denominator = pow(currI,2) + pow(currQ,2);
+			demod.push_back(numerator/denominator);
+		}
+
+		p_i = currI;
+		p_q = currQ;
+	}
+}
